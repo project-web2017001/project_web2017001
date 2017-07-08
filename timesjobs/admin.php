dad4e272876c45b0f48b83a24a6d1a172dad1e12
satyam <stym06@github.com>
2017-07-08 22:36:51 +0530

    Added Admin Panel

---
+++ timesjobs/admin.php
@@ -0,0 +1,71 @@
+
+<!DOCTYPE html>
+<html lang='en'>
+	<title>Admin Login</title>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/superhero/bootstrap.min.css">
+		<style>
+			#form-div{
+				width:300px;
+				height: 400px;
+				margin-top: 7%;
+				margin-left: 40%;
+				padding-top: 20px;
+				padding-left: 20px;
+				/*border-top:1px solid #eee;
+				border-bottom:1px solid #eee;*/
+			}
+			#submit{
+				margin-left: 33%;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container-fluid">
+		<ol class="breadcrumb" style="background: transparent;position: relative;top: 176px;">
+			<li><a href="index.php">Home</a></li>
+			<li>Admin Panel</li>
+		</ol>
+			<div id="form-div">
+			<h2 class="text-warning text-center">Admin Login</h2>
+			<form action="" method="post" class="form-horizontal">
+				<div class="form-group">
+					<label for="name">Email:</label>
+					<input type="text" class="form-control" id='email' name='email'>
+				</div>
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input type="password" class="form-control" id='password' name='password'>
+				</div>
+				<div class="row">
+					<input type="submit" class="btn btn-lg btn-primary" id="submit" value="Submit">
+				</div>
+			</form>
+		</div>
+		</div>
+
+
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		<script>
+			$(document).ready(function(){
+				$('#submit').click(function(e){
+					var email=$('#email').val();
+					var password=$('#password').val();
+					
+					e.preventDefault();
+					if(email!='' && password!=''){
+					$.ajax({
+						url: 'action.php',
+						method: 'POST',
+						data: {admin_login:1,email:email,password:password},
+						success: function(data){
+							window.location.replace('manage.php');
+						}
+					})
+				}
+				})
+			})
+		</script>
+	</body>
+</html>
